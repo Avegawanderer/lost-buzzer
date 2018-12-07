138b231290cb03c49a3f9d98af9b4e97a1358ee7
borozdin.a <borozdin.a@milandr.ru>
2018-12-07 18:03:29 +0300

    Add simple PWM setup

--- source/main.cpp
+++ source/main.cpp
@@ -10,6 +10,39 @@ void delay(u16 time)
     }
 }
 
+
+
+void setup_pwm(void)
+{
+    TIM1_DeInit();
+    TIM1_TimeBaseInit(0, TIM1_COUNTERMODE_CENTERALIGNED1, 4095, 0);
+    
+    TIM1_OC1Init( TIM1_OCMODE_PWM2, 
+                  TIM1_OUTPUTSTATE_ENABLE, 
+                  TIM1_OUTPUTNSTATE_ENABLE,
+                  1500, 
+                  TIM1_OCPOLARITY_LOW, 
+                  TIM1_OCNPOLARITY_HIGH, 
+                  TIM1_OCIDLESTATE_SET,
+                  TIM1_OCNIDLESTATE_RESET
+    ); 
+    
+    TIM1_OC2Init( TIM1_OCMODE_PWM2, 
+                  TIM1_OUTPUTSTATE_ENABLE, 
+                  TIM1_OUTPUTNSTATE_ENABLE, 
+                  1000,
+                  TIM1_OCPOLARITY_LOW, 
+                  TIM1_OCNPOLARITY_HIGH, 
+                  TIM1_OCIDLESTATE_SET, 
+                  TIM1_OCNIDLESTATE_RESET
+    );
+  
+
+    TIM1_Cmd(ENABLE);
+    TIM1_CtrlPWMOutputs(ENABLE);
+}
+
+
 int main()
 {
     /* Fmaster = 16MHz */
